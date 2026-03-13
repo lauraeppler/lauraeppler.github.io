1e8a9405bc3f03d7cb0f99a5883c0550aeadb7b5
MariaLStefan <103122411+MariaLStefan@users.noreply.github.com>
2026-03-13 13:07:05 +0100

    fix(Home): smooth scroll to Projects - jumping to headlines instead of project-cards

--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -6,9 +6,9 @@ import Project from "./Project";
 
 const ProjectList = () => {
   const projects = useFetchProjectContent();
-  console.log("projects", projects);
+ 
   return (
-  <section className="projects">
+  <section className="projects" id="projects">
     <h2>Meine Projekte</h2>
     <div className="project-list">
       {projects.map((project, idx) => (
