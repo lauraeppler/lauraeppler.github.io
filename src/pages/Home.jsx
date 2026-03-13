1b69399f08d94d6c76f258f64ad7c38eb3c184dd
MariaLStefan <103122411+MariaLStefan@users.noreply.github.com>
2026-03-13 23:30:18 +0100

    refactor(Layout): use global grid - all sections will be set into the wrapper per default - opt out to xl-wrapper and fullwidth is possible for every element - use new Layout for ProjectDetail - use new Layout for About - use new Layout for Footer - use new Layout for Projects - use new Layout for Hero - use new Layout for Header - show Header right-aligned - remove inline-padding for content-sections - set all sections to display: grid for layout to work

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,15 @@
-import About from '../components/About';
-import ProjectList from '../components/ProjectList';
-import Contact from '../components/Contact';
+import About from "../components/About";
+import ProjectList from "../components/ProjectList";
+import Contact from "../components/Contact";
 
 const Home = () => {
-    return (
-        <> 
-            <About />
-            <ProjectList />
-            <Contact /> 
-        </>
-    )
+  return (
+    <>
+      <About />
+      <ProjectList />
+      <Contact />
+    </>
+  );
 };
 
 export default Home;
