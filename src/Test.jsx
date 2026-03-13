7867e897864dfbe630677885aeb71531aeb3f4a7
MariaLStefan <103122411+MariaLStefan@users.noreply.github.com>
2026-03-13 23:47:02 +0100

    chore: remove testing

--- src/Test.jsx
+++
@@ -1,9 +0,0 @@
-const Test = () => {
-    return (
-        <div>
-            <h1>Testseite</h1>
-            <p>Dies ist eine Testseite für die Projektübersicht.</p>
-        </div>
-    );
-}
-export default Test;
