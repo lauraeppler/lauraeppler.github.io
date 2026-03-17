56121ed954ba95dfd3ea97c891c2369bc0baa0aa
Laura Eppler <le028@hdm-stuttgart.de>
2026-03-17 15:59:47 +0100

    update localmessenger.mdx and add fourways icons

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -3,6 +3,11 @@ import "@/styles/Contact.css";
 const Contact = () => (
   <section className="contact" id="contact">
     <h2>Kontakt</h2>
+    <p>Lass uns gerne vernetzen - ich freue mich über einen Austausch</p>
+
+    <p> Offen auf</p>
+
+    
   </section>
 );
 
