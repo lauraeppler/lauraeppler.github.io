406438c20189b0a7c3548c9a10c1a4b7adb7007f
MariaLStefan <103122411+MariaLStefan@users.noreply.github.com>
2026-03-20 15:24:12 +0100

    fix: font & primary colors accessible

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,7 +2,7 @@ import "@/styles/About.css";
 
 const About = () => (
   <section className="about section-container xl-wrapper" id="about">
-    <h2>Accessibility isn't an add-on. It's good UX.</h2>
+    <h1>Accessibility isn&apos;t an add-on. It&apos;s good UX.</h1>
     <p>
       Willkommen auf meinem Portfolio. Hier findest du ausgewählte Projekte und
       Einblicke in meine Arbeit an der Schnittstelle von digitaler
